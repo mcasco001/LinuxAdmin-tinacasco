410d826173f8dcb6f2f9d88ac272b77a1fe48151
LinuxAdmin-tinacasco <mcasco@DESKTOP-ONOIS56.localdomain>
2025-02-06 23:18:36 -0600

    Restore utils.sh

---
+++ utils.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env bash
+
+echo "utils just ran!"
+alias ll='ls-laFh'
+alias list="ls -laFh"
+alias grep="grep --color=auto"
+
+alias gh='history | grep' #List command history based on filter
+alias df="df -h" #Display disk usage
+ 
+#Adds any changes on the main branch and commits, then push the update
+alias gpush='git add . && git commit -m "Updated" && git push'
+
+function git-acp() {
+	git add .
+	git commit -m 'This was committed with git-acp'
+	echo "git push would've run now!"
+	#git push
+
+}
