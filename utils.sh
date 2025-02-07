4a60d5cdd0a3617794ba3c8b08a517c4e394bbeb
LinuxAdmin-tinacasco <mcasco@DESKTOP-ONOIS56.localdomain>
2025-02-06 23:42:05 -0600

    Commit update

--- utils.sh
+++ utils.sh
@@ -1,19 +1,30 @@
 #! /usr/bin/env bash
 
 echo "utils just ran!"
+alias ll="ls -laFh"
 alias list="ls -laFh"
 alias grep="grep --color=auto"
 
 alias gh='history | grep' #List command history based on filter
-alias df="df -h" #Display disk usage
- 
+alias df="df -h" #Display disk usage 
 #Adds any changes on the main branch and commits, then push the update
 alias gpush='git add . && git commit -m "Updated" && git push'
+alias sutils="source utils.sh" #This will run the update on the utils.sh, instead of typing source utils.sh everytime
+
+
+#function git-acp() {
+#	git add .
+#	git commit -m 'This was committed with git-acp'
+#	echo "git push would've run now!"
+#	#git push }
 
 function git-acp() {
+	if [-z "$1"]; then
+		echo "Error: Type in a commit message!"
+		return 1
+	fi
 	git add .
-	git commit -m 'This was committed with git-acp'
+	git commit -m "$1" || return 3
 	echo "git push would've run now!"
-	#git push
-
-}
+	git push && echo "Push successful" || (echo "Failed to push :(" && return 3)
+	}
